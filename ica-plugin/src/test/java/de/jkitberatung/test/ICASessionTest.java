433605b4322a28552899df6e5a1f044057179eab
Sampath Kondam <samfromco@yahoo.com>
2017-05-11 20:59:13 +0000

    Added test class

---
+++ ica-plugin/src/test/java/de/jkitberatung/test/ICASessionTest.java
@@ -0,0 +1,13 @@
+
+package de.jkitberatung.test;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ICASessionTest {
+   @Test
+   public void testICASession() {
+      Assert.assertEquals("Hello! World", "Hello! World", "Success!!!");
+   }
+
+   
+}
